66c3fb4d624a4ec630ef97f17524da0f3a462c60
Yanukovich Yan <yan99-09@mail.ru>
2022-11-18 10:04:25 +0300

    [master]: added unversioned files

---
+++ quiz_ts/frontend/src/components/choice.ts
@@ -0,0 +1,103 @@
+import {CustomHttp} from "../services/custom-http.ts";
+import config from "../../config/config";
+import {Auth} from "../services/auth.ts";
+
+export class Choice {
+    constructor() {
+        this.quizzes = [];
+        this.testResults = null
+
+        this.init()
+    }
+
+    async init() {
+        try {
+            const result = await CustomHttp.request(`${config.host}/tests`, 'GET')
+            if (result) {
+                if (result.error) {
+                    throw new Error(result.error)
+                }
+                this.quizzes = result
+            }
+        } catch (e) {
+            return console.log(e)
+        }
+
+        const userInfo = Auth.getUserInfo()
+        if (userInfo) {
+            try {
+                const results = await CustomHttp.request(`${config.host}/tests/results?userId=${userInfo.userId}`, 'GET')
+                if (results) {
+                    if (results.error) {
+                        throw new Error(results.error)
+                    }
+                    this.testResults = results
+                }
+            } catch (e) {
+                return console.log(e)
+            }
+        }
+        this.processQuizzes()
+    }
+
+    processQuizzes() {
+        const choiceOptionsElement = document.getElementById('choice-options')
+
+        if (this.quizzes && this.quizzes.length > 0) {
+            this.quizzes.forEach(quiz => {
+                const that = this
+
+                const choiceOptionElement = document.createElement('div')
+                choiceOptionElement.className = 'choice-option'
+                choiceOptionElement.setAttribute('data-id', quiz.id)
+                choiceOptionElement.onclick = function () {
+                    that.chooseQuiz(this)
+                }
+
+                const choiceOptionTextElement = document.createElement('div')
+                choiceOptionTextElement.className = 'choice-option-text'
+                choiceOptionTextElement.innerText = quiz.name
+
+                const choiceOptionArrowElement = document.createElement('div')
+                choiceOptionArrowElement.className = 'choice-option-arrow'
+
+                const choiceOptionImageElement = document.createElement('img')
+                choiceOptionImageElement.setAttribute('src', '/images/arrow.png')
+                choiceOptionImageElement.setAttribute('alt', 'arrow')
+
+                choiceOptionArrowElement.appendChild(choiceOptionImageElement)
+                choiceOptionElement.appendChild(choiceOptionTextElement)
+                choiceOptionElement.appendChild(choiceOptionArrowElement)
+                choiceOptionsElement.appendChild(choiceOptionElement)
+
+                if (this.testResults) {
+                    this.testResults.forEach(backResults => {
+                        if (backResults.testId === quiz.id) {
+                            const choiceOptionsResultTest = document.createElement('div')
+                            choiceOptionsResultTest.className = 'choice-options-result-test'
+
+                            const choiceOptionsResultTestText = document.createElement('div')
+                            choiceOptionsResultTestText.className = 'choice-options-result-test-text'
+                            choiceOptionsResultTestText.innerText = 'Результат'
+
+                            const choiceOptionsResultTestScore = document.createElement('div')
+                            choiceOptionsResultTestScore.className = 'choice-options-result-test-text'
+                            choiceOptionsResultTestScore.innerText = `${backResults.score}/${backResults.total}`
+
+                            choiceOptionsResultTest.appendChild(choiceOptionsResultTestText)
+                            choiceOptionsResultTest.appendChild(choiceOptionsResultTestScore)
+                            choiceOptionElement.appendChild(choiceOptionsResultTest)
+                        }
+                    })
+                }
+            })
+        }
+    }
+
+    chooseQuiz(element) {
+        const dataId = element.getAttribute('data-id')
+        if (dataId) {
+            location.href = '#/test?id=' + dataId
+        }
+    }
+}
---
+++ quiz_ts/frontend/src/router.ts
@@ -0,0 +1,139 @@
+import {Form} from "./components/form";
+import {Choice} from "./components/choice";
+import {Test} from "./components/test";
+import {Result} from "./components/result";
+import {Auth} from "./services/auth";
+import {RightAnswers} from "./components/right-answers"
+import {RouteType} from "./types/route.type";
+import {UserInfoType} from "./types/user-info.type";
+
+export class Router {
+
+    readonly contentElement: HTMLElement | null
+    readonly stylesElement: HTMLElement | null
+    readonly titleElement: HTMLElement | null
+    readonly profileElement: HTMLElement | null
+    readonly profileFullNameElement: HTMLElement | null
+
+    private routes: RouteType[]
+
+    constructor() {
+        this.contentElement = document.getElementById('content')
+        this.stylesElement = document.getElementById('styles')
+        this.titleElement = document.getElementById('page-title')
+        this.profileElement = document.getElementById('profile')
+        this.profileFullNameElement = document.getElementById('profile-full-name')
+
+        this.routes = [
+            {
+                route: '#/',
+                title: 'Главная',
+                template: 'templates/index.html',
+                styles: 'styles/index.css',
+                load: () => {
+                }
+            },
+            {
+                route: '#/signup',
+                title: 'Регистрация',
+                template: 'templates/signup.html',
+                styles: 'styles/form.css',
+                load: () => {
+                    new Form('signup');
+                }
+            },
+            {
+                route: '#/login',
+                title: 'Вход в систему',
+                template: 'templates/login.html',
+                styles: 'styles/form.css',
+                load: () => {
+                    new Form('login');
+                }
+            },
+            {
+                route: '#/choice',
+                title: 'Выбор теста',
+                template: 'templates/choice.html',
+                styles: 'styles/choice.css',
+                load: () => {
+                    new Choice();
+                }
+            },
+            {
+                route: '#/test',
+                title: 'Прохождение теста',
+                template: 'templates/test.html',
+                styles: 'styles/test.css',
+                load: () => {
+                    new Test();
+                }
+            },
+            {
+                route: '#/result',
+                title: 'Результаты',
+                template: 'templates/result.html',
+                styles: 'styles/result.css',
+                load: () => {
+                    new Result();
+                }
+            },
+            {
+                route: '#/right',
+                title: 'Результат прохождения теста',
+                template: 'templates/right-answers.html',
+                styles: 'styles/right-answers.css',
+                load: () => {
+                    new RightAnswers();
+                }
+            },
+        ]
+    }
+
+
+    public async openRoute(): Promise<void> {
+        const urlRoute: string = window.location.hash.split('?')[0]
+
+        if (urlRoute === '#/logout') {
+            await Auth.logout()
+            window.location.href = '#/'
+            return
+        }
+
+        const newRoute: RouteType | undefined = this.routes.find(item => {
+            return item.route === urlRoute
+        });
+
+        if (!newRoute) {
+            window.location.href = '#/'
+            return
+        }
+
+        if (!this.contentElement || !this.stylesElement
+            || !this.profileElement || !this.profileFullNameElement || !this.profileElement) {
+            if (urlRoute === '#/') {
+                return
+            } else {
+                window.location.href = '#/'
+                return
+            }
+        }
+
+        this.contentElement.innerHTML =
+            await fetch(newRoute.template).then(response => response.text())
+        this.stylesElement.setAttribute('href', newRoute.styles)
+
+        const userInfo: UserInfoType | null = Auth.getUserInfo()
+        const accessInfo: string | null = localStorage.getItem(Auth.accessTokenKey)
+        if (userInfo && accessInfo) {
+            this.profileElement.style.display = 'flex'
+            this.profileFullNameElement.innerText = userInfo.fullName
+        } else {
+            this.profileElement.style.display = 'none '
+        }
+
+        newRoute.load()
+
+        if (this.titleElement) this.titleElement.innerText = newRoute.title
+    }
+}
